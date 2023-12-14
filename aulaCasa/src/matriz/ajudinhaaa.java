1f4d7641eadb442acbdfaf74d18a0ae6f79a9bc2
bianca <bianca_vaz@estudante.sesisenai.org.br>
2023-12-13 23:56:02 -0300

    editado13.12

---
+++ aulaCasa/src/matriz/ajudinhaaa.java
@@ -0,0 +1,25 @@
+package matriz;
+
+public class ajudinhaaa {
+	
+
+	
+public static void main (String [] args) {
+		
+		for ( int linha = 0; linha < 5; linha++ ) {
+	        for ( int coluna = 0; coluna < 5; coluna++ ) {
+	       
+	        }
+	        
+	     }
+
+		for ( int linha = 0; linha < 5; linha++ ) {
+	        for ( int coluna = 0; coluna < 5; coluna++ ) {
+	            System.out.print ( "  ( " + linha + " ) ( " + coluna + " )  " );
+	        }
+	        System.out.println ();
+	    }
+	    
+	}
+
+}
